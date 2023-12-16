be493f7bfca9048edb2690587a8072baa94fbb97
Dicainum <mentokcnn@gmail.com>
2023-12-16 18:23:17 +0300

    commit

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!")
-const name = readlineSync.question('What is your name? ');
-console.log(`Hello, ${name}!`);
+import startEvenGame from '../src/brain-even.js';
+
+startEvenGame();
