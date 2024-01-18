ca75eaf30c95e499214306e0c23a02ba21723245
Dicainum <146223178+Dicainum@users.noreply.github.com>
2024-01-18 19:05:47 +0300

    Update brain-gcd.js

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -2,19 +2,22 @@ import startEngine from './index.js';
 import { generateRandomNumber } from './helps.js';
 
 const rule = 'Find the greatest common divisor of given numbers.';
-const minRange = 1;
-const maxRange = 50;
+const min = 1;
+const max = 50;
 
 const getNOD = (num1, num2) => (num2 === 0 ? num1 : getNOD(num2, num1 % num2));
 
 const startRound = () => {
-  const number1 = generateRandomNumber(minRange, maxRange);
-  const number2 = generateRandomNumber(minRange, maxRange);
+  const number1 = generateRandomNumber(min, max);
+  const number2 = generateRandomNumber(min, max);
 
-  const question = `${number1} ${number2}`;
-  const correctAnswer = getNOD(number1, number2).toString();
+  const expression = `${number1} ${number2}`;
+  const answer = getNOD(number1, number2).toString();
 
-  return [question, correctAnswer];
+  return {
+    answer,
+    expression,
+  };
 };
 
 const runBrainNodGame = () => startEngine(rule, startRound);
