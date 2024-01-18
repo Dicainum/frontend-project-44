6ec1149cc6bd584553a6ccc1a1896ebd8dd0dda9
Dicainum <146223178+Dicainum@users.noreply.github.com>
2024-01-18 15:41:29 +0300

    commit

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const startMainGame = () => {
+const startHello = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
-export default startMainGame;
+export default startHello;
