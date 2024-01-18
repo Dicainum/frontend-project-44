5150149a66c02cbdf9176a61a5c5872503640d9b
Dicainum <146223178+Dicainum@users.noreply.github.com>
2024-01-18 15:40:32 +0300

    commit

--- src/brain-calc.js
+++ src/brain-calc.js
@@ -1,4 +1,4 @@
-import startEngine from './engine.js';
+import startEngine from './index.js';
 
 const rule = 'What is the result of the expression?';
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
@@ -7,41 +7,41 @@ const limit = {
   max: 100,
 };
 const operator = {
-    plus: '+',
-    minus: '-',
-    prod: '*',
+  plus: '+',
+  minus: '-',
+  prod: '*',
 };
 
 const calculateExpr = (firstNum, secondNum, oper) => {
-    switch (oper) {
-      case operator.plus:
-        return firstNum + secondNum;
-      case operator.minus:
-        return firstNum - secondNum;
-      case operator.prod:
-        return firstNum * secondNum;
-      default:
-        throw new Error(`Unknown operator: '${oper}'!`);
-    }
-  };
+  switch (oper) {
+    case operator.plus:
+      return firstNum + secondNum;
+    case operator.minus:
+      return firstNum - secondNum;
+    case operator.prod:
+      return firstNum * secondNum;
+    default:
+      throw new Error(`Unknown operator: '${oper}'!`);
+  }
+};
 
 const getCalcGameOptions = () => {
-    const randA = getRandomNum(limit.min, limit.max);
-    const randB = getRandomNum(limit.min, limit.max);
-    const randOper = Object.values(operator)[getRandomNum(0, Object.keys(operator).length - 1)];
-  
-    const expression = `${randA} ${randOper} ${randB}`;
-  
-    const value = calculateExpr(randA, randB, randOper);
+  const randA = getRandomNum(limit.min, limit.max);
+  const randB = getRandomNum(limit.min, limit.max);
+  const randOper = Object.values(operator)[getRandomNum(0, Object.keys(operator).length - 1)];
 
-    const answer = String(value);
-  
-    return {
-      answer,
-      expression,
-    };
+  const expression = `${randA} ${randOper} ${randB}`;
+
+  const value = calculateExpr(randA, randB, randOper);
+
+  const answer = String(value);
+
+  return {
+    answer,
+    expression,
   };
+};
 
-const startCalcGame = () => startEngine(rule, getEvenGameOptions);
+const startCalcGame = () => startEngine(rule, getCalcGameOptions);
 
 export default startCalcGame;
