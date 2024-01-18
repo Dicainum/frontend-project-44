cf9f2dc324e330b7e1c577cec35d2c8b3ec1df38
Dicainum <146223178+Dicainum@users.noreply.github.com>
2024-01-18 15:54:30 +0300

    brain-prime.js

---
+++ src/brain-prime.js
@@ -0,0 +1,22 @@
+import startEngine from '../index.js';
+import { generateQuestionAndAnswer } from '../utils.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const minRange = 1;
+const maxRange = 50;
+
+const checkIsPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+
+const startRound = () => {
+  const [question, correctAnswer] = generateQuestionAndAnswer(minRange, maxRange, checkIsPrime);
+  return [question, correctAnswer];
+};
+
+const runBrainPrimeGame = () => startEngine(rule, startRound);
+
+export default runBrainPrimeGame;
